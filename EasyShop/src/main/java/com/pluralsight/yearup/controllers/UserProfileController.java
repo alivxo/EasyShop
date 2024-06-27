971acbbac0a67dacb1a32695ad7b9633b9bec5c3
alivxo <avsal0218@gmail.com>
2024-06-27 10:15:56 -0400

    User Profile Controller

---
+++ EasyShop/src/main/java/com/pluralsight/yearup/controllers/UserProfileController.java
@@ -0,0 +1,61 @@
+package com.pluralsight.yearup.controllers;
+
+
+import com.pluralsight.yearup.data.ProfileDao;
+import com.pluralsight.yearup.data.UserDao;
+import com.pluralsight.yearup.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+@Secured({"ROLE_ADMIN", "ROLE_USER"})
+public class UserProfileController {
+
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
+
+    @Autowired
+    public UserProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("")
+    public Profile getProfile(Principal principal) {
+        try {
+            // Get the currently logged-in username
+            String userName = principal.getName();
+            // Find database user by username
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            return profileDao.getProfileById(userId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    @PutMapping("")
+    public int updateProfile(Principal principal, @RequestBody Profile profile) {
+        try {
+            // Get the currently logged-in username
+            String userName = principal.getName();
+            // Find database user by username
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            return profileDao.updateProfile(userId, profile);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+}
--- EasyShop/src/main/java/com/pluralsight/yearup/data/ProfileDao.java
+++ EasyShop/src/main/java/com/pluralsight/yearup/data/ProfileDao.java
@@ -6,4 +6,8 @@ import com.pluralsight.yearup.models.Profile;
 public interface ProfileDao
 {
     Profile create(Profile profile);
+
+    org.springframework.context.annotation.Profile getProfileById(int userId);
+
+    int updateProfile(int userId, org.springframework.context.annotation.Profile profile);
 }
