50f8e483361397dc7f6e4f4c6ec99abd21c60a6c
alivxo <avsal0218@gmail.com>
2024-06-27 23:20:15 -0400

    added code

--- EasyShop/src/main/java/com/pluralsight/yearup/configurations/DatabaseConfig.java
+++ EasyShop/src/main/java/com/pluralsight/yearup/configurations/DatabaseConfig.java
@@ -1,10 +1,14 @@
 package com.pluralsight.yearup.configurations;
 
+import com.pluralsight.yearup.data.*;
+import com.pluralsight.yearup.data.mysql.*;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig
@@ -16,6 +20,37 @@ public class DatabaseConfig
     {
         return basicDataSource;
     }
+
+    @Bean
+    public ProductDao productDao(DataSource dataSource) {
+        return new MySqlProductDao(dataSource);
+    }
+
+    @Bean
+    public CategoryDao categoryDao(DataSource dataSource) {
+        return new MySqlCategoryDao(dataSource);
+    }
+
+    @Bean
+    public ShoppingCartDao shoppingCartDao(DataSource dataSource) {
+        return new MySqlShoppingCartDao(dataSource);
+    }
+
+    @Bean
+    public ProfileDao profileDao (DataSource dataSource) {
+        return new MySqlProfileDao(dataSource);
+    }
+
+    @Bean
+    public UserDao userDao (DataSource dataSource) {
+        return new MySqlUserDao(dataSource);
+    }
+
+    @Bean
+    public OrderDao orderDao(DataSource dataSource) {
+        return new MySqlOrderDao(dataSource);
+    }
+
 
     @Autowired
     public DatabaseConfig(@Value("${datasource.url}") String url,
